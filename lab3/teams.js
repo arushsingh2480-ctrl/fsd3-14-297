e992029d979e6be2992b56078740550cb1baec86
arushsingh2480 <arushsingh2480-ctrl@gmail.com>
2026-09-10 14:50:57 +0530

    lab4 commit

---
+++ lab3/teams.js
@@ -0,0 +1,18 @@
+const teams = [
+    {
+        id: 1,
+        name: "Team Alpha"
+    },
+    {
+        id: 2,
+        name: "Team Beta"
+    },
+    {
+        id: 3,
+        name: "Team Gamma"
+    }
+];
+
+export const getAllTeams = () => {
+    return teams;
+};
